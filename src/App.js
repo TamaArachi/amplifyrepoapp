204b10e62dc5d8f5d7fc38ad6291c34ee50095d9
TamaArachi <51135325+TamaArachi@users.noreply.github.com>
2020-08-04 15:17:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {withAuthenticator,AmplifySignOut} from '@aws-amplify/ui-react'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello this is Mona's React App deployed in AWS Amplify</h1>
+        <h2>We now have Auth enabled!! </h2>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,8 +19,9 @@ function App() {
           Learn React
         </a>
       </header>
+    <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
